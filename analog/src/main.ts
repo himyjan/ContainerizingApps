cfd24ce365b732b92797ec5ce57686dae96ac8d3
himyjan <51815522+himyjan@users.noreply.github.com>
2022-11-24 10:01:49 +0800

    update package version

--- analog/src/main.ts
+++ analog/src/main.ts
@@ -1,11 +1,9 @@
 import './polyfills';
-import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideFileRouter } from '@analogjs/router';
 
 import { AppComponent } from './app/app.component';
 
-if (import.meta.env.PROD) {
-  enableProdMode();
-}
-
-bootstrapApplication(AppComponent);
+bootstrapApplication(AppComponent, {
+  providers: [provideFileRouter()],
+});
