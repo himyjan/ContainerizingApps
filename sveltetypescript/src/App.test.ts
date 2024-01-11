0580797fbc0af6f7f955c74036b69805b048cdd8
himyjan <51815522+himyjan@users.noreply.github.com>
2024-01-11 08:31:18 +0800

    upload code

---
+++ sveltetypescript/src/App.test.ts
@@ -0,0 +1,32 @@
+import { tick } from 'svelte'
+import Counter from './lib/Counter.svelte'
+
+let host: HTMLElement
+
+afterEach(() => {
+  host.remove()
+})
+
+test('mount component', async () => {
+  host = document.createElement('div')
+  host.setAttribute('id', 'host')
+  document.body.appendChild(host)
+  const instance = new Counter({ target: host, props: { } })
+  expect(instance).toBeTruthy()
+  expect(host.innerHTML).toContain('count is 0')
+  expect(host.innerHTML).toMatchSnapshot()
+  const btn = host.getElementsByTagName('button')[0]
+  btn.click() // or btn.dispatchEvent(new window.Event('click', { bubbles: true }))
+  await tick()
+  expect(host.innerHTML).toContain('count is 1')
+  btn.click()
+  await tick()
+  expect(host.innerHTML).toContain('count is 2')
+})
+
+/*
+//TODO improvements
+    - generic way to create svelte components from import ( helper utility or library )
+    - generic api for interacting with components
+    - alternatives to expect with innerHTML
+ */
