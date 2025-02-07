6e9bca52dcee0d0200adfb700ffb5a755a439afc
himyjan <beforeyayayaya@gmail.com>
2025-02-07 15:02:11 +0800

    update package version

--- one/app/_layout.tsx
+++ one/app/_layout.tsx
@@ -1,18 +1,18 @@
 import { Slot } from 'one'
 
+/**
+ * The root _layout.tsx filters <html /> and <body /> out on native
+ */
+
 export default function Layout() {
   return (
-    <>
-      {typeof document !== 'undefined' && (
-        <>
-          <meta charSet="utf-8" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
-          <link rel="icon" href="/favicon.svg" />
-        </>
-      )}
+    <html lang="en-US">
+      <meta charSet="utf-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
+      <link rel="icon" href="/favicon.svg" />
 
       <Slot />
-    </>
+    </html>
   )
 }
