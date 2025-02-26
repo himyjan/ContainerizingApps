114befa7392c7dda9d2c42a75fc5b61fdc3c8dab
himyjan <beforeyayayaya@gmail.com>
2025-02-26 15:42:09 +0800

    update package version

--- analog/vite.config.ts
+++ analog/vite.config.ts
@@ -5,22 +5,19 @@ import analog from '@analogjs/platform';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  publicDir: 'src/assets',
   build: {
     target: ['es2020'],
   },
   resolve: {
     mainFields: ['module'],
   },
-  plugins: [analog()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['src/test.ts'],
-    include: ['**/*.spec.ts'],
-    reporters: ['default'],
-  },
-  define: {
-    'import.meta.vitest': mode !== 'production',
-  },
+  plugins: [
+    analog({
+      ssr: false,
+      static: true,
+      prerender: {
+        routes: [],
+      },
+    }),
+  ],
 }));
