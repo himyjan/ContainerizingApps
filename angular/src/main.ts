ea09c73f1e2fb739d6f3ec7e85a78d4dcc369a87
himyjan <beforeyayayaya@gmail.com>
2026-03-27 16:23:41 +0800

    upload code

--- angular/src/main.ts
+++ angular/src/main.ts
@@ -1,9 +1,6 @@
-import { importProvidersFrom } from '@angular/core';
-import { AppComponent } from './app/app.component';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from '../app/app.config';
+import { App } from '../app/app';
 
-
-bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule)]
-})
-  .catch(err => console.error(err));
+bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err));
